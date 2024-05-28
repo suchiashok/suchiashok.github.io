34d88ef89a20e341c6a812dab4f0b5148f258415
suchi ashok <suchiashok.97@gmail.com>
2024-05-27 22:06:30 -0400

    Initial Commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+var typed3 = new Typed('.typed', {
+  strings: ['Hi, I\'m Suchita'],
+  typeSpeed: 30,
+  smartBackspace: true,
+  cursorChar: "<span class='type-cursor'>_</b>",
+  autoInsertCss: true,
+  loop: false
+});
