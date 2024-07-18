eda04e7f26b99f896a8747bf0faf5c1d8504ae67
suchi ashok <suchiashok.97@gmail.com>
2024-07-18 16:58:19 -0400

    modifying the home page intro

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 var typed3 = new Typed('.typed', {
-  strings: ['Hi, I\'m Suchita'],
+  strings: ['Hi, I\'m Suchita',],
   typeSpeed: 30,
   smartBackspace: true,
   cursorChar: "<span class='type-cursor'>_</b>",
